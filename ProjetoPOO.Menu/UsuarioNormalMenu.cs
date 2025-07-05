0aee13cfffb49873b0c67d6e56f550c3bcb5f8e7
gabriel.bado <gbado2006@gmail.com>
2025-07-05 04:20:01 -0300

    user normal

--- ProjetoPOO.Menu/UsuarioNormalMenu.cs
+++ ProjetoPOO.Menu/UsuarioNormalMenu.cs
@@ -6,8 +6,18 @@ namespace ProjetoPOO.Menu;
 public  class UsuarioNormalMenu
 {
 
-    public UsuarioNormalMenu()
+    private readonly Cliente _clienteService;
+    private readonly FornecedorService _fornecedorService;
+    private readonly TransportadoraService _transportadoraService;
+
+    private readonly ProdutoService _produtoService;
+
+    public UsuarioNormalMenu(Cliente clienteService, FornecedorService fornecedorService, TransportadoraService transportadoraService, ProdutoService produtoService)
     {
+        _clienteService = clienteService ?? throw new ArgumentNullException(nameof(clienteService));
+        _transportadoraService = transportadoraService ?? throw new ArgumentNullException(nameof(transportadoraService));
+        _produtoService = produtoService ?? throw new ArgumentNullException(nameof(produtoService));
+        _fornecedorService = fornecedorService ?? throw new ArgumentNullException(nameof(fornecedorService));
 
     }
     public  void MenuUsuario()
@@ -19,7 +29,8 @@ public  class UsuarioNormalMenu
             Console.WriteLine("1 - Consultar Fornecedor");
             Console.WriteLine("2 - Consultar Transportadora");
             Console.WriteLine("3 - Consultar Produto");
-            Console.WriteLine("4 - Sair");
+            Console.WriteLine("4 - deve ser coisa de compra");
+            Console.WriteLine("5 - Sair");
             Console.Write("Digite a opção: ");
 
             opcao = int.Parse(Console.ReadLine()!);
@@ -36,6 +47,9 @@ public  class UsuarioNormalMenu
                     //produtoService.Consultar();
                     break;
                 case 4:
+                    Console.WriteLine("Voltar Tela de Login...");
+                    break;
+                case 5:
                     Console.WriteLine("Voltar Tela de Login...");
                     break;
                 default:
